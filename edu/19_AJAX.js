4b0a09450b7334362b034bed0f07aa5b44bca84f
yoonhee-jung <bcvcx0011@gmail.com>
2025-09-01 18:00:04 +0900

    250901 promise ~ AJAX

--- edu/19_AJAX.js
+++ edu/19_AJAX.js
@@ -11,10 +11,21 @@ axios.get(url)
     const container = document.querySelector('.container');
     container.appendChild(newImage);
 
-    
   });
 
 })
 .catch(error => {
   console.log(error);
 });
+
+// async function test(url) {
+//   try {
+//     const newImage = document.createElement('img');
+//     newImage.setAttribute('src', data.download_url);
+//     newImage.setAttribute('width', '300px');
+//     const container = document.querySelector('.container');
+//     container.appendChild(newImage);
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
